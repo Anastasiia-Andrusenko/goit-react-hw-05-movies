bb7a65d873b1df21d88167c9922a215b373347a1
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2024-09-09 15:11:50 +0300

    Update ScrollButtonHorizontal.jsx

--- src/components/ScrollButton/ScrollButtonHorizontal.jsx
+++ src/components/ScrollButton/ScrollButtonHorizontal.jsx
@@ -1,23 +1,19 @@
-
 import classNames from 'classnames';
-// import { useRef, useState } from 'react';
-import {IoIosArrowDropright, IoIosArrowDropleft} from 'react-icons/io';
+import { IoIosArrowDropright, IoIosArrowDropleft } from 'react-icons/io';
 import css from './ScrollButtonHorizontal.module.css';
 
-
 export const ScrollButtonRight = () => {
-  // const [visible, setVisible] = useState(false);
-
-  return <div className={classNames(css.btn, css.right)} >
-    <IoIosArrowDropright/>
-  </div>
-}
+  return (
+    <div className={classNames(css.btn, css.right)}>
+      <IoIosArrowDropright />
+    </div>
+  );
+};
 
 export const ScrollButtonLeft = () => {
-  // const [visible, setVisible] = useState(false);
-
-
-  return <div className={classNames(css.btn, css.left)} >
-    <IoIosArrowDropleft/>
-  </div>
-}
+  return (
+    <div className={classNames(css.btn, css.left)}>
+      <IoIosArrowDropleft />
+    </div>
+  );
+};
